68ae27ab0b15d9eaf91fa9270af938a4f625b270
tsengfs <963988788@qq.com>
2021-07-08 11:25:27 +0800

    静态页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title','Weibo App') - Laravel 新手入门教程！</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="offset-md-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
